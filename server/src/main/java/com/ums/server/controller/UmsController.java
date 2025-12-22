21916a7f58e40271860da20b54f839fecf6ff1ee
Akash Biswas <akashbiswas.271999@gmail.com>
2025-12-22 14:13:52 +0530

    Move the react-endpoint configutration to a separate file.

--- server/src/main/java/com/ums/server/controller/UmsController.java
+++ server/src/main/java/com/ums/server/controller/UmsController.java
@@ -5,18 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ClientController {
-
-
-    @GetMapping(value = "/")
-    public String home(){
-        return "home";
-    }
+public class UmsController {
 
     @GetMapping(value = "/ums/**")
     public String reactApplicationHandler(){
         return "forward:/index.html";
     }
-
-
 }
